0e91eadc7342227e18281fbedd2e0dde941c46a0
codeavenger456 <36369573+codeavenger456@users.noreply.github.com>
2021-04-30 13:15:20 -0400

    Add files via upload

--- cnn_svm/utils/data.py
+++ cnn_svm/utils/data.py
@@ -54,7 +54,6 @@ def load_tfds(
 
     return train_features, test_features, train_labels, test_labels
 
-
 def create_dataset(
     features: np.ndarray,
     labels: np.ndarray,
@@ -88,4 +87,52 @@ def create_dataset(
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     dataset = dataset.batch(batch_size)
     dataset = dataset.shuffle(features.shape[1])
+    dataset.features = features
+    dataset.labels = labels
+    
     return dataset
+    
+class DataSet(object):
+
+    def __init__(self, images, labels):
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0 # How many epochs have passed
+        self._index_in_epoch = 0 # Index in an epoch
+        self._num_examples = images.shape[0]# refers to the total number of samples of training data
+    
+    def next_batch(self, batch_size, fake_data=False, shuffle=True):
+        start = self._index_in_epoch 
+        # Shuffle for the first epoch The first epoch needs shuffle
+        if self._epochs_completed == 0 and start == 0 and shuffle:
+            perm0 = np.arange(self._num_examples)  #Generate an np.array of all sample lengths
+            np.random.shuffle(perm0)
+            self._images = self._images[perm0]
+            self._labels = self._labels[perm0]
+            # Go to the next epoch
+
+
+        if start + batch_size > self._num_examples: #epoch end and beginning of next epoch
+            # Finished epoch
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start  # In the end, there is not enough batch and a few remaining
+            images_rest_part = self._images[start:self._num_examples]
+            labels_rest_part = self._labels[start:self._num_examples]
+            # Shuffle the data
+            if shuffle: 
+                perm = np.arange(self._num_examples)
+                np.random.shuffle(perm)
+                self._images = self._images[perm]
+                self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end = self._index_in_epoch
+            images_new_part = self._images[start:end] 
+            labels_new_part = self._labels[start:end]
+            return np.concatenate((images_rest_part, images_new_part), axis=0) , np.concatenate((labels_rest_part, labels_new_part), axis=0)    
+        else:  # Except for the first epoch, and the beginning of each epoch, the remaining batch processing methods
+            self._index_in_epoch += batch_size # start = index_in_epoch
+            end = self._index_in_epoch #end is very simple, it is index_in_epoch plus batch_size 
+            return self._images[start:end], self._labels[start:end] #In data x,y
